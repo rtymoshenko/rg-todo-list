decc95f4b3689a87740abbb5b09ed01744a5e3b4
Roman Tymoshenko <tymoshenkorv@gmail.com>
2016-09-04 00:22:11 +0300

    Added to Tasks#complete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,36 @@
 class TasksController < ApplicationController
 
-before_action :find_project
+  before_action :find_project
+  before_action :find_task, except: [:create]
 
   def create
-    binding.pry
+    # binding.pry
     @task = @project.tasks.create(task_params)
     redirect_to @project
   end
 
   def destroy
-    @task = @project.tasks.find(params[:id])
     if @task.destroy
-      flash[:succss] = "Task is deleted"
+      flash[:success] = "Task is deleted"
     else
       flash[:error] = "Task could NOT be deleted."
     end
     redirect_to @project
   end
 
+  def complete
+    @task.update_attribute(:status, true)
+    redirect_to @project, notice: "Task is completed"
+  end
+
   private
 
   def find_project
      @project = Project.find(params[:project_id])
+  end
+
+  def find_task
+    @task = @project.tasks.find(params[:id])
   end
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root "projects#index"
 
   resources :projects do
-    resources :tasks
+    resources :tasks do
+      member do
+        patch :complete
+      end
+    end
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
